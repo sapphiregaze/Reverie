27526931432935e5d09efee2220a2b46f34a0b47
SapphireGaze <jasminehuang2048@outlook.com>
2024-02-03 13:10:24 -0800

    Added Get User Profile Via JWT Token:  - Added getting user profile from jwt token

--- backend/src/router/user.router.ts
+++ backend/src/router/user.router.ts
@@ -3,8 +3,33 @@ import express from "express";
 import User from "../database/models/user.model";
 import Dream from "../database/models/dream.model";
 
+import { authenticate } from "../lib/auth";
+
 const UserRouter: express.Router = express.Router();
 UserRouter.use(express.json());
+
+UserRouter.get("/", async (req: express.Request, res: express.Response) => {
+  try {
+    const token: string =
+      (req.headers.authorization && req.headers.authorization.split(" ")[1]) ||
+      "";
+
+    const id: number | null = await authenticate(token);
+
+    const user: any = await User.findOne({ where: { id: id } });
+    const dreams: any = await Dream.findAll({ where: { user_id: id } });
+
+    res.status(200).json({
+      username: user.username,
+      noctara_points: user.noctara_points,
+      streaks: user.streak,
+      dreams: dreams,
+    });
+  } catch (err) {
+    res.status(501).json({ message: (err as Error).message });
+    console.error(err);
+  }
+});
 
 UserRouter.get("/:id", async (req: express.Request, res: express.Response) => {
   try {
