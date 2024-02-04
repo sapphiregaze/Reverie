220e8f76adbb06eed28cc206b3aff295817a4b10
SapphireGaze <jasminehuang2048@outlook.com>
2024-02-03 21:05:27 -0800

    Finished Task Frontend API:  - Hooked up tasks frontend with backend

--- frontend/src/app/tasks/page.tsx
+++ frontend/src/app/tasks/page.tsx
@@ -1,10 +1,43 @@
+"use client";
 import { Navbar } from "@/components/Navbar";
+import { getTasks } from "@/lib/api";
+import { useEffect, useState } from "react";
 
 export default function TasksPage() {
+  const [tasks, setTasks] = useState<string[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const tasks: string[] = await getTasks();
+        setTasks(tasks);
+      } catch (err) {
+        console.error("Error fetching recipes:", err);
+        return (
+          <p className="text-center text-2xl">
+            Error loading tasks. Please try again later.
+          </p>
+        );
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <>
-      <main className="flex min-h-screen flex-col items-center justify-between p-12">
-        Tasks
+      <main className="flex min-h-screen flex-col justify-start p-12">
+        <div className="text-2xl">
+          To Do
+          <span className="mx-2 rounded-lg bg-purple-300 px-2 text-xl">
+            {tasks.length}
+          </span>
+        </div>
+        <div>
+          {tasks.map((task, index) => (
+            <div key={index}>{task}</div>
+          ))}
+        </div>
       </main>
       <Navbar />
     </>
--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -3,6 +3,19 @@ import { ErrorMessage, Token, User } from "./types";
 const host: string =
   process.env.NEXT_PUBLIC_API_HOST || "http://127.0.0.1:8000";
 
+export const getTasks = async (): Promise<string[]> => {
+  const response: Response = await fetch(`${host}/api/task`);
+
+  if (!response.ok) {
+    const error: ErrorMessage = await response.json();
+    throw new Error(`Failed to fetch data: ${error.error}`);
+  }
+
+  const data: string[] = await response.json();
+
+  return data;
+};
+
 export const login = async (
   username: string,
   password: string,
