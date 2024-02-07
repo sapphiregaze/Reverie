0e8ecb3307dbdbb7639dff185aa4faafa313ede4
SapphireGaze <jasminehuang2048@outlook.com>
2024-02-07 02:52:35 -0800

    Nativewind Setup + Landing Page Finished:  - Setup nativewind (tailwind for react native)  - Recreated landing page with react native

--- reverie/App.js
+++ reverie/App.js
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Alert, Image, Pressable, Text, View } from "react-native";
+import { NativeWindStyleSheet } from "nativewind";
+
+NativeWindStyleSheet.setOutput({
+  default: "native",
+});
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className="flex-1 items-center justify-center bg-[#FCFAF6] p-12">
+      <Text className="text-5xl text-center font-ui-sans-serif">
+        Level up your <Text className="font-bold">daily routine</Text> together.
+      </Text>
+      <Image source={require("./assets/hero.png")} />
+      <Pressable
+        className="m-2 bg-[#6E4AFF] w-full rounded-3xl p-2"
+        onPress={() => Alert.alert("Login button pressed")}
+      >
+        <Text className="text-xl text-center text-[#FCFAF6]">Login</Text>
+      </Pressable>
+      <Pressable
+        className="m-2 bg-[#FCFAF6] w-full rounded-3xl p-2 border-[#6E4AFF] border-2"
+        onPress={() => Alert.alert("Register button pressed")}
+      >
+        <Text className="text-xl text-center text-[#6E4AFF]">Register</Text>
+      </Pressable>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
