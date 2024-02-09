d42a686407194619d9ee9436ba81c22ef3efaaa4
SapphireGaze <jasminehuang2048@outlook.com>
2024-02-08 21:42:26 -0800

    Finished Login and Register UI: - Finished UI for login and register screen

--- mobile/screens/LandingScreen.js
+++ mobile/screens/LandingScreen.js
@@ -4,7 +4,7 @@ import { View, Text, Image, Pressable, Alert } from "react-native";
 const LandingScreen = ({ navigation }) => {
   return (
     <View className="flex-1 items-center justify-center bg-[#FCFAF6] p-12">
-      <Text className="text-5xl text-center font-ui-sans-serif">
+      <Text className="text-5xl text-center">
         Level up your <Text className="font-bold">daily routine</Text> together.
       </Text>
       <Image source={require("../assets/hero.png")} />
@@ -12,13 +12,17 @@ const LandingScreen = ({ navigation }) => {
         className="m-2 bg-[#6E4AFF] w-full rounded-3xl p-2"
         onPress={() => navigation.navigate("Login")}
       >
-        <Text className="text-xl text-center text-[#FCFAF6]">Login</Text>
+        <Text className="text-xl font-semibold text-center text-[#FCFAF6]">
+          Login
+        </Text>
       </Pressable>
       <Pressable
         className="m-2 bg-[#FCFAF6] w-full rounded-3xl p-2 border-[#6E4AFF] border-2"
         onPress={() => navigation.navigate("Register")}
       >
-        <Text className="text-xl text-center text-[#6E4AFF]">Register</Text>
+        <Text className="text-xl  font-semibold text-center text-[#6E4AFF]">
+          Register
+        </Text>
       </Pressable>
     </View>
   );
--- mobile/screens/LoginScreen.js
+++ mobile/screens/LoginScreen.js
@@ -1,9 +1,44 @@
-import { View, Text } from "react-native";
+import { View, Text, Pressable, TextInput } from "react-native";
+import Icon from "react-native-vector-icons/AntDesign";
 
 const LoginScreen = ({ navigation }) => {
   return (
-    <View className="flex-1 items-center justify-center bg-[#FCFAF6] p-12">
-      <Text>Login</Text>
+    <View className="flex-1 bg-[#FCFAF6] p-12">
+      <Pressable
+        className="mt-6 -ml-4"
+        onPress={() => navigation.navigate("Landing")}
+      >
+        <Text className="text-xl text-gray-900 font-semibold">
+          <Icon name="back" size={24} color="#111827" />
+          &nbsp;Go Back
+        </Text>
+      </Pressable>
+      <View className="flex flex-col items-center justify-center">
+        <Text className="text-3xl text-center font-semibold mt-32 mb-8">
+          Login
+        </Text>
+        <View className="w-full mb-6">
+          <Text className="ml-2 font-semibold my-2">Username</Text>
+          <TextInput
+            className="bg-gray-200 rounded-3xl w-full p-4 shadow-2xl mb-2"
+            placeholder="Enter your username"
+          />
+          <Text className="ml-2 font-semibold my-2">Password</Text>
+          <TextInput
+            className="bg-gray-200 rounded-3xl w-full p-4 shadow-2xl mb-2"
+            placeholder="Enter your password"
+            secureTextEntry={true}
+          />
+        </View>
+        <Pressable
+          className="m-2 bg-[#FCFAF6] w-full rounded-3xl p-2 border-[#6E4AFF] border-2"
+          onPress={() => navigation.navigate("Login")}
+        >
+          <Text className="text-xl font-semibold text-center text-[#6E4AFF]">
+            Login
+          </Text>
+        </Pressable>
+      </View>
     </View>
   );
 };
